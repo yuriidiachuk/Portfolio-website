203a881738920ed2781420287e589aa327432f94
yurdia817@gmail.com <yurdia817@gmail.com>
2024-12-23 14:58:17 +0100

    avatar styles changed

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,13 +1,13 @@
 import Image from "next/image";
 const Avatar = () => {
   return (
-    <div className="hidden lg:flex lg:max-w-none -scale-x-100">
+    <div className="hidden lg:flex lg:max-w-none ">
       <Image
         src={"/sticker2.png"}
         width={537}
         height={478}
         alt=""
-        className="translate-z-0 w-full h-full  "
+        className="w-full h-full translate-z-0 "
       />
     </div>
   );
